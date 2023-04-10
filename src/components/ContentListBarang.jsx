a0193f0da39c045682c50cf6b58864f06209426e
FaixFarisky <130310605+FaixFarisky@users.noreply.github.com>
2023-04-10 21:26:31 +0700

    ListBarang
    Memperbaiki halaman list barang

--- src/components/ContentListBarang.jsx
+++ src/components/ContentListBarang.jsx
@@ -100,7 +100,7 @@ const ContentListBarang = () => {
                                     Rp. 250.000
                                 </td>
                                 <td class="px-6 py-4">
-                                    <a onClick={() => setHijau ("Beras Punel Jember")} href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                                    <a onClick={() => setHijau ("Beras Punel Surabaya")} href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
                                 </td>
                                 <td class="px-6 py-4">
                                     <a href="#" class="font-medium text-blue-600 dark:text-red-500 hover:underline">Hapus</a>
